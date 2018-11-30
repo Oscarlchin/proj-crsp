702099d9be8e47eac613074bfb9c418e84742df7
carstencww <carstencww@gmail.com>
2018-12-01 02:07:03 +0800

    finish get all events

--- backend/useractions.js
+++ backend/useractions.js
@@ -1,8 +1,12 @@
 var jwtuser = require('./_helpers/jwt').jwtuser;
 var db = require('./_helpers/db');
+var dbcall = require('./_helpers/dbcall');
 module.exports = function (app){
   app.get("/useraction/events",jwtuser,function(req,res){
-    res.send(db.events);
+    dbcall.getAllEvents().then(function(events){
+      res.json(events);
+    },function(err){
+      res.json(err);
+    });
   });
-
 }
