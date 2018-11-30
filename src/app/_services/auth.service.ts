6df06f033ecbef31b2ab6a7d98835ced308b4d26
carstencww <carstencww@gmail.com>
2018-11-30 18:17:18 +0800

    add useraction interface

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { config } from '../_globals';
+
 @Injectable()
 export class AuthService {
     constructor(private http: HttpClient) { }
---
+++ src/app/_services/useraction.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { config } from '../_globals';
+import { User, Event } from '../_models';
+
+@Injectable()
+export class UseractionService {
+  constructor(private http: HttpClient) { }
+  getevent() {
+    return this.http.get<Event[]>(`${config.BASE_API_URL}/useraction/events`);
+  }
+  favevent(program_id: number) {
+    return this.http.put<User>(`${config.BASE_API_URL}/useraction/favevent`, {program_id: program_id} );
+  }
+  leavecomment(user_comment: string) {
+    return this.http.put<Event>(`${config.BASE_API_URL}/useraction/leavecomment`, {user_comment: user_comment} );
+  }
+
+}
