f9b3f74ca12f75bd697db33e3c141cc60d891f63
carstencww <carstencww@gmail.com>
2018-11-30 23:15:56 +0800

    add user get events, modified event model

--- backend/_helpers/db.js
+++ backend/_helpers/db.js
@@ -1,69 +1,88 @@
 var events = [
-  { program_name: "aeroboicsdfsf",
-    district: "en district",
-    venue: "en venue",
-    start_date: "start_date",
-    end_date:"",
-    dayinweek:"",
-    start_time:"",
-    end_time:"",
-    type_name:"",
-    fee: "",
-    quota: "",
-    quota_left: "",
-    min_age: "",
-    max_age: "",
-    url: ""
+  { program_id: 40484679,
+    program_name: "Aerobic Dance Training Course",
+    district: "Kwai Tsing",
+    venue: "Tsing Yi Southwest Sports Centre",
+    start_date: "2018-12-03",
+    end_date:"2019-01-21",
+    dayinweek:"Mon,Wed (excl.24/12,26/12,31/12)",
+    start_time:"18:00",
+    end_time:"19:00",
+    type_name:"Aerobic Dance",
+    fee: 65,
+    quota: 25,
+    quota_left: 0,
+    min_age: 14,
+    max_age: 99,
+    url: "https://www.lcsd.gov.hk/clpss/en/search/leisure/srchCommRecretSprtProgsDtls.do?programCode=40484679"
   },
-  { program_name: "aeroboicsdfsf",
-    district: "en district",
-    venue: "en venue",
-    start_date: "start_date",
-    end_date:"",
-    dayinweek:"",
-    start_time:"",
-    end_time:"",
-    type_name:"",
-    fee: "",
-    quota: "",
-    quota_left: "",
-    min_age: "",
-    max_age: "",
-
+  { program_id: 40484686,
+    program_name: "Badminton Training Course",
+    district: "Kwai Tsing",
+    venue: "Tsing Yi Southwest Sports Centre",
+    start_date: "2019-01-02",
+    end_date:"2019-03-13",
+    dayinweek:"Wed (excl.06/02)",
+    start_time:"19:00",
+    end_time:"21:00",
+    type_name:"Badminton",
+    fee: 118,
+    quota: 24,
+    quota_left: 24,
+    min_age: 7,
+    max_age: 99,
+    url: "https://www.lcsd.gov.hk/clpss/en/search/leisure/srchCommRecretSprtProgsDtls.do?programCode=40484686"
   },
-  { program_name: "aeroboicsdfsf",
-    district: "en district",
-    venue: "en venue",
-    start_date: "start_date",
-    end_date:"",
-    dayinweek:"",
-    start_time:"",
-    end_time:"",
-    type_name:"",
-    fee: "",
-    quota: "",
-    quota_left: "",
-    min_age: "",
-    max_age: "",
-
+  { program_id: 40484743,
+    program_name: "Children Dance Training Course",
+    district: "Kwai Tsing",
+    venue: "Tsing Yi Southwest Sports Centre",
+    start_date: "2019-01-12",
+    end_date:"2019-03-16",
+    dayinweek:"Sat",
+    start_time:"14:00",
+    end_time:"16:00",
+    type_name:"Children Dance",
+    fee: 60,
+    quota: 20,
+    quota_left: 20,
+    min_age: 4,
+    max_age: 6,
+    url: "https://www.lcsd.gov.hk/clpss/en/search/leisure/srchCommRecretSprtProgsDtls.do?programCode=40484743"
   },
-  { program_name: "aeroboicsdfsf",
-    district: "en district",
-    venue: "en venue",
-    start_date: "start_date",
-    end_date:"",
-    dayinweek:"",
-    start_time:"",
-    end_time:"",
-    type_name:"",
-    fee: "",
-    quota: "",
-    quota_left: "",
-    min_age: "",
-    max_age: "",
-
-  },
+  { program_id: 40485943,
+    program_name: "Rugby TC (18+)",
+    district: "Yuen Long",
+    venue: "Tin Shui Wai Park(5-a-side Soccer Pitch)",
+    start_date: "2019-01-08",
+    end_date:"2019-03-19",
+    dayinweek:"Tue (excl.05/02)",
+    start_time:"20:00",
+    end_time:"22:00",
+    type_name:"Rugby",
+    fee: 40,
+    quota: 20,
+    quota_left: 20,
+    min_age: 18,
+    max_age: 99,
+    url:"https://www.lcsd.gov.hk/clpss/en/search/leisure/srchCommRecretSprtProgsDtls.do?programCode=40485943"
+    },
 ]
+
+var eventcomments = [
+  {
+    program_id: 1,
+    comment: [{username:"ffff", content: "ffffuu"}, {username:"fdf", content: "ffffuu"}]
+  },{
+    program_id: 2,
+    comment: [{username:"aaaa", content: "ffffuu"}, {username:"fcf", content: "ffffuu"}]
+  },{
+    program_id: 3,
+    comment: [{username:"bbbb", content: "ffffuu"}, {username:"fbf", content: "ffffuu"}]
+  },{
+    program_id: 4,
+    comment: [{username:"cccc", content: "ffffuu"}, {username:"faf", content: "ffffuu"}]
+  }];
 
 var users = {
   userone: {
@@ -82,5 +101,6 @@ var users = {
 
 module.exports = {
   users: users,
-  events: events
+  events: events,
+  eventcomments: eventcomments
 }
--- backend/useractions.js
+++ backend/useractions.js
@@ -1,5 +1,8 @@
 var jwtuser = require('./_helpers/jwt').jwtuser;
-
+var db = require('./_helpers/db');
 module.exports = function (app){
+  app.get("/useraction/events",jwtuser,function(req,res){
+    res.send(db.events);
+  });
 
 }
