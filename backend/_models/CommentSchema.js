dfdde3fdcb6484bdbce93c96db16330e1268f9af
carstencww <carstencww@gmail.com>
2018-12-02 22:52:54 +0800

    update comment schema

--- backend/_models/CommentSchema.js
+++ backend/_models/CommentSchema.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var CommentSchema = mongoose.Schema({
   program_id: { type: Number, required: true, unique: true },
-  eventcomments: [{type: String}]
+  eventcomments: [{
+    username: String,
+    usercomment: String }]
 });
 
 module.exports = mongoose.model('Comment',CommentSchema);
