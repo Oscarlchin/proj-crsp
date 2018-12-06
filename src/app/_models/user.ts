3bd9ead613f695d3f46baa96b5a02d9bf385d74a
carstencww <carstencww@gmail.com>
2018-12-06 16:21:55 +0800

    fixed bugs in FE model

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -1,5 +1,5 @@
 export class User {
     username: string;
     password: string;
-    favevents: string[];
+    favevents: number[];
 }
--- src/app/_services/useraction.service.ts
+++ src/app/_services/useraction.service.ts
@@ -10,15 +10,15 @@ export class UseractionService {
     return this.http.get<Event[]>(`${config.BASE_API_URL}/useraction/events`) ;
   }
   likeevent(username: string , program_id: number) {
-    return this.http.put<User>(`${config.BASE_API_URL}/useraction/` + username + '/likeevent', {program_id: program_id} );
+    return this.http.put(`${config.BASE_API_URL}/useraction/` + username + '/likeevent', {program_id: program_id} );
   }
 
   unlikeevent(username: string , program_id: number) {
-    return this.http.put<User>(`${config.BASE_API_URL}/useraction/` + username + '/unlikeevent', {program_id: program_id} );
+    return this.http.put(`${config.BASE_API_URL}/useraction/` + username + '/unlikeevent', {program_id: program_id} );
   }
 
   leavecomment(user_comment: string) {
-    return this.http.put<Event>(`${config.BASE_API_URL}/useraction/leavecomment`, {user_comment: user_comment} );
+    return this.http.put<EventComment>(`${config.BASE_API_URL}/useraction/leavecomment`, {user_comment: user_comment} );
   }
 
   getcomment(program_id: number) {
