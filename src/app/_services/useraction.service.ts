a2ad852153959a79971199b20f256aadfb1d231d
carstencww <carstencww@gmail.com>
2018-12-06 16:16:18 +0800

    modify useraction according to BE

--- src/app/_services/useraction.service.ts
+++ src/app/_services/useraction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { config } from '../_globals';
-import { User, Event } from '../_models';
+import { User, Event, EventComment } from '../_models';
 
 @Injectable()
 export class UseractionService {
@@ -9,10 +9,19 @@ export class UseractionService {
   getevent() {
     return this.http.get<Event[]>(`${config.BASE_API_URL}/useraction/events`) ;
   }
-  favevent(program_id: number) {
-    return this.http.put<User>(`${config.BASE_API_URL}/useraction/favevent`, {program_id: program_id} );
+  likeevent(username: string , program_id: number) {
+    return this.http.put<User>(`${config.BASE_API_URL}/useraction/` + username + '/likeevent', {program_id: program_id} );
   }
+
+  unlikeevent(username: string , program_id: number) {
+    return this.http.put<User>(`${config.BASE_API_URL}/useraction/` + username + '/unlikeevent', {program_id: program_id} );
+  }
+
   leavecomment(user_comment: string) {
     return this.http.put<Event>(`${config.BASE_API_URL}/useraction/leavecomment`, {user_comment: user_comment} );
+  }
+
+  getcomment(program_id: number) {
+    return this.http.get<EventComment>(`${config.BASE_API_URL}/useraction/comment/` + program_id);
   }
 }
