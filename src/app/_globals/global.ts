99333e2ed7414ebd80a69707e992ae7468f6f098
carstencww <carstencww@gmail.com>
2018-12-16 22:33:31 +0800

    modify config

--- src/app/_globals/global.ts
+++ src/app/_globals/global.ts
@@ -3,6 +3,5 @@
 // Name: Fong Kee Win     SID: 1155100567
 
 export const config = Object.freeze({
-  BASE_API_URL: 'http://localhost:3000',  // http://18.222.134.186:3000
-  //  ... more variables
+  BASE_API_URL: 'api server address',
 });
